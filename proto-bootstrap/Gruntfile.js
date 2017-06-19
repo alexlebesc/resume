bd1516e3c79f6313b185902390a5a8bafb970278
Alex Le Bescond <alex.lebescond@gmail.com>
2017-06-19 08:45:00 +0200

    metttre developpeur web full stack en gras

---
+++ proto-bootstrap/Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function (grunt)
+{
+
+  // Project configuration.
+  grunt.initConfig(
+  {
+    pkg: grunt.file.readJSON('package.json'),
+
+    // COPY ===================================/
+    copy:
+    {
+      all:
+      {
+        files: [
+          {
+            expand: true,
+            src: ['css/**', 'img/**', 'js/**', 'node_modules/**', '*.html'],
+            dest: '<%= pkg.build %>/'
+          }
+        ]
+      }
+    }
+  });
+
+
+
+  // DEPENDENT PLUGINS =========================/
+
+  // Load the plugin that provides the "copy" task.
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+
+  // TASKS =========================/
+
+  // Build task
+  grunt.registerTask('build', ['copy:all']);
+};
