be0de8a91d0ec2b59d964f7fed7f1f58bf5670f5
Alex Le Bescond <alex.lebescond@gmail.com>
2017-06-24 14:36:30 +0200

    experience style

--- proto-bootstrap/js/main.js
+++ proto-bootstrap/js/main.js
@@ -2,24 +2,9 @@
  * Created by alex on 08/05/17.
  */
 
-console.log('test');
+$('.full-height').each(function(){
+     var el = $(this);
+     var row = el.closest('.row');
 
-var fullHeight = function() {
-    console.log('hey');
-
-   $('.full-height').each(function(){
-        var el = $(this);
-        var row = el.closest('.row');
-
-        el.height(row.height());
-    });
-};
-
-fullHeight();
-
-
-$('.collapse')
-    .on('show.bs.collapse', function () { fullHeight(); })
-    .on('shown.bs.collapse', function () { fullHeight(); })
-    .on('hide.bs.collapse', function () { fullHeight(); })
-    .on('hidden.bs.collapse', function () { fullHeight(); });
+     el.height(row.height());
+ });
