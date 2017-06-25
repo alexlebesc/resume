ce6b96ec8c1bacd143e374b2e28be901e17f475a
Alex Le Bescond <alex.lebescond@gmail.com>
2017-06-25 22:42:15 +0200

    fix style and mistake

--- proto-bootstrap/js/main.js
+++ proto-bootstrap/js/main.js
@@ -2,9 +2,33 @@
  * Created by alex on 08/05/17.
  */
 
-$('.full-height').each(function(){
-     var el = $(this);
-     var row = el.closest('.row');
+var fullHeight = function() {
+    $('.full-height').each(function(){
+        var el = $(this);
+        var row = el.closest('.row');
 
-     el.height(row.height());
- });
+        el.height(row.height());
+    });
+};
+
+// media query change
+var WidthChange = function(mq) {
+    if (mq.matches) {
+        fullHeight();
+    }
+};
+
+// media query event handler
+if (matchMedia) {
+    var mq = window.matchMedia( "(min-width: 992px)" );
+    mq.addListener(WidthChange);
+    WidthChange(mq);
+} else {
+    fullHeight();
+}
+
+
+
+
+
+
